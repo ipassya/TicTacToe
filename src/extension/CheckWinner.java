e113456001a17be35f1c2d6f2c950da30624ac9a
Ihza Passya <46826137+ipassya@users.noreply.github.com>
2020-02-17 17:01:40 +0700

    Update CheckWinner.java

--- src/extension/CheckWinner.java
+++ src/extension/CheckWinner.java
@@ -5,27 +5,27 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CheckWinner {
-    public String checkWinner(ArrayList<Integer> playerPositions, ArrayList<Integer> cpuPositions) {
-        // membuat list array dengan asList
-        List topRow = Arrays.asList(1, 2, 3);
-        List midRow = Arrays.asList(4, 5, 6);
-        List botRow = Arrays.asList(7, 8, 9);
-        List leftCol = Arrays.asList(1, 4, 7);
-        List midCol = Arrays.asList(2, 5, 8);
-        List rightCol = Arrays.asList(3, 6, 9);
-        List cross1 = Arrays.asList(1, 5, 9);
-        List cross2 = Arrays.asList(7, 5, 3);
+    // membuat list array dengan asList
+    private List topRow = Arrays.asList(1, 2, 3);
+    private List midRow = Arrays.asList(4, 5, 6);
+    private List botRow = Arrays.asList(7, 8, 9);
+    private List leftCol = Arrays.asList(1, 4, 7);
+    private List midCol = Arrays.asList(2, 5, 8);
+    private List rightCol = Arrays.asList(3, 6, 9);
+    private List cross1 = Arrays.asList(1, 5, 9);
+    private List cross2 = Arrays.asList(7, 5, 3);
 
+    public String checkWinner(ArrayList<Integer> playerPositions, ArrayList<Integer> cpuPositions) {
         // menambahkan list kedalam winning
         List<List> winning = new ArrayList<List>();
-        winning.add(topRow);
-        winning.add(midRow);
-        winning.add(botRow);
-        winning.add(leftCol);
-        winning.add(midCol);
-        winning.add(rightCol);
-        winning.add(cross1);
-        winning.add(cross2);
+        winning.add(this.topRow);
+        winning.add(this.midRow);
+        winning.add(this.botRow);
+        winning.add(this.leftCol);
+        winning.add(this.midCol);
+        winning.add(this.rightCol);
+        winning.add(this.cross1);
+        winning.add(this.cross2);
 
         // Pengecekan positions
         for (List l : winning) {
